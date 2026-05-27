5aef919c59f7529aa207d959a21c34fabd3cbf13
uae9447008-uA <uae9447008@gmail.com>
2026-05-27 16:29:28 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
-print("roulette bot is running")
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from collections import Counter
+
+BOT_TOKEN = "8964099452:AAFoGE9KDxhSylwYY8xBqZ46LfYnqg6bMWk"
+
+async def start(update, context):
+    await update.message.reply_text("🎰 أرسل أرقام الروليت")
+
+async def analyze(update, context):
+    try:
+        nums = [int(x) for x in update.message.text.split()]
+        nums = [n for n in nums if 0 <= n <= 36]
+
+        count = Counter(nums)
+
+        msg = "🔥 أكثر الأرقام:\n"
+
+        for n, c in count.most_common(5):
+            msg += f"{n} = {c} مرات\n"
+
+        await update.message.reply_text(msg)
+
+    except:
+        await update.message.reply_text("❌ أرسل أرقام فقط")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, analyze))
+
+app.run_polling()
